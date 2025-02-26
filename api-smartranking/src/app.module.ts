a0cb2ae177ab4d39c8c9647e9a0ebf0d81760347
fabiocaettano <fabio.caettano74@gmail.com>
2025-02-26 11:04:56 -0300

    feat: instalado as bibliotecas @nestjs/cofig @nestjs/mongoose mongoose

--- api-smartranking/src/app.module.ts
+++ api-smartranking/src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { JogadoresModule } from './jogadores/jogadores.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [JogadoresModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(`${process.env.MONGODB}`, {
+      serverSelectionTimeoutMS: 60000, // 60 segundos
+    }),
+    JogadoresModule],
   controllers: [],
   providers: [],
 })
