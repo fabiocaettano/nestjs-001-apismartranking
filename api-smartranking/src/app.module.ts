3f9afd875d09e5744baa2996de2dc322adbadd98
fabiocaettano <fabio.caettano74@gmail.com>
2025-02-26 11:14:29 -0300

    feat: configurado o forroot do MongooseModule para buscar o caminho do banco no arquivo env, não versionado

--- api-smartranking/src/app.module.ts
+++ api-smartranking/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { JogadoresModule } from './jogadores/jogadores.module';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot(),    
     MongooseModule.forRoot(`${process.env.MONGODB}`, {
       serverSelectionTimeoutMS: 60000, // 60 segundos
     }),
