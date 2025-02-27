f6b5745a41f4b56abf3424f10f1d45776bf609c7
fabiocaettano <fabio.caettano74@gmail.com>
2025-02-26 21:06:33 -0300

    feat: implementado validação no handle @Post, e configuração no dto

--- api-smartranking/src/jogadores/jogadores.controller.ts
+++ api-smartranking/src/jogadores/jogadores.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CriarJogadorDto } from './dto/criar-jogador.dto';
 import { JogadoresService } from './jogadores.service';
 import { Jogador } from './interfaces/jogador.interface';
@@ -9,6 +9,7 @@ export class JogadoresController {
     constructor(private readonly jogadoresService: JogadoresService){}
     
     @Post()
+    @UsePipes(ValidationPipe)
     async criarAtualizarJogador(
         @Body() atualizadorJogadorDto : CriarJogadorDto
     ) {
